050e337259c4b0b2419a1f5256a97695c4f12950
Ayazzsaifi <104493436+Ayazzsaifi@users.noreply.github.com>
2026-07-02 19:41:12 +0530

    1st full stack Auth

--- week-6.1/week-6.2/auth-app.js
+++ week-6.1/week-6.2/auth-app.js
@@ -1,106 +1,132 @@
 const express = require('express');
-const jwt=require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const path = require('path');
 
-const app=express();
+const app = express();
 
 // To use a JWT 1st create a JWT secret - 
-const JWT_SECRET="pass123";
+const JWT_SECRET = "pass123";
 
 app.use(express.json());// to take the input from body 
-const users=[];
-
-function auth(){
-    
-}
+const users = [];
 
 
-app.post('/signup',function(req,res){
-    const username=req.body.username;
-    const password=req.body.password;
-    
+app.get("/",function(req,res){
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+})
+
+
+app.post('/signup', logger, function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
     // we should check if a person is already of this name
 
-    const existingUser = users.find(function(u){
+    const existingUser = users.find(function (u) {
         return u.username === username;
     });
 
-    if(existingUser){
-         return res.json({
-            message:"User exist with this username"
+    if (existingUser) {
+        return res.json({
+            message: "User exist with this username"
         })
     }
 
-    else{ 
-    users.push({
-        username:username,
-        password:password
-    })}
+    else {
+        users.push({
+            username: username,
+            password: password
+        })
+    }
 
 
     res.json({
-        message:"You have successfully signup, you can login now"
+        message: "You have successfully signup, you can login now"
     })
 
 });
 
+function logger(req, res, next) {
+    console.log(req.method)
+    next();
+}
 
-app.post('/signin',function(req,res){
-    const username=req.body.username;
-    const password=req.body.password;
 
-    const foundUser=users.find(function(u){
-        if(u.username == username && u.password == password){
+app.post('/signin', logger, function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const foundUser = users.find(function (u) {
+        if (u.username == username && u.password == password) {
             return true
         }
-        else{
-            return false 
+        else {
+            return false
         }
     })
 
 
     // we have to send back a token when user signIn
 
-    if(foundUser){
+    if (foundUser) {
 
-        const token=jwt.sign({
+        const token = jwt.sign({
             username
-        },JWT_SECRET);
+        }, JWT_SECRET);
 
 
         res.json({
-            token:token
+            token: token
         })
     }
-    else{
+    else {
         res.json({
-            message:"Credentials are incorrect"
+            message: "Credentials are incorrect"
         })
     }
 
 });
 
+function auth(req, res, next) {
+    const token = req.headers.token;
 
-app.get("/me",function(req,res){
-    const token=req.headers.token;
-
-    const decodedData=jwt.verify(token,JWT_SECRET);
-
-    if(decodedData.username){
-        const foundUser=users.find(function(u){
-            return u.username===decodedData.username
+    if(!token){
+        return res.status(401).json({
+            message:"youre not logged In"
         });
+    }
 
-        if(!foundUser){
-            return res.json({
-                error:"Invalid"
-            })
-        }
+    
+    try{
+        const decodedData = jwt.verify(token, JWT_SECRET);
+        req.username = decodedData.username;
+        next()
+    }
 
-        res.json({
-            username:foundUser.username,
-            password:foundUser.password
+    catch(err){
+         return res.status(401).json({ message: "Invalid or expired token" });
+    }
+}
+
+
+
+app.get("/me", auth, logger, function (req, res) {
+
+    const foundUser = users.find(function (u) {
+        return u.username === req.username
+    });
+
+    if (!foundUser) {
+        return res.json({
+            error: "Invalid"
         })
     }
+
+    res.json({
+        username: foundUser.username,
+        password:foundUser.password
+    })
+
 
 })
 
